34a3ec8210e3fa5066c6284c53c2855a1e0e773b
可及 <3183346767@qq.com>
2022-02-02 14:08:07 +0800

    排序微服务实现

--- pr-sorting-engine-9004/src/main/java/com/jjyu/controller/RuleSortController.java
+++ pr-sorting-engine-9004/src/main/java/com/jjyu/controller/RuleSortController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("prSorting/rule")
 public class RuleSortController {
-    //1.获取某一算法的排序列表
+    //1.获取某一规则对PR的排序列表
     //2.当有其它对PR的操作时，要实时更新相关数据
 
 }
