648752420bc722f2505e4ebb32ee2a5f303f36ae
可及 <3183346767@qq.com>
2022-02-04 19:29:35 +0800

    PR管理与项目管理同时开发

--- project-manage-9003/src/main/java/com/jjyu/ProjectManageApplication.java
+++ project-manage-9003/src/main/java/com/jjyu/ProjectManageApplication.java
@@ -1,9 +1,11 @@
 package com.jjyu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.jjyu.mapper")
 public class ProjectManageApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectManageApplication.class, args);
