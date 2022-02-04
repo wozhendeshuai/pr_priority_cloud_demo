97a6ee8ad8a82e75554fe7a5e195289b7cba0d30
可及 <3183346767@qq.com>
2022-02-04 19:42:44 +0800

    PR管理与项目管理同时开发

--- pr-manage-9002/src/main/java/com/jjyu/controller/PRSortController.java
+++ pr-manage-9002/src/main/java/com/jjyu/controller/PRSortController.java
@@ -1,26 +1,21 @@
 package com.jjyu.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.jjyu.utils.DateTimeUtil;
 import com.jjyu.utils.ResultForFront;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Api(value = "prManage/sort", tags = {"PR基础操作"})
+@Api(value = "prManage/sort", tags = {"PR排序操作"})
 @Slf4j
 @RestController
 @RequestMapping("prSorting/alg")
-public class PRBaseController {
+public class PRSortController {
 
-    //1.新建PR
+    //1.查看排序列表
     @ApiOperation(value = "获取某一算法的排序无序列表", notes = "listAll")
     @GetMapping("listAll")
     public ResultForFront getAll(@RequestParam("repoName") String repoName,
@@ -29,9 +24,8 @@ public class PRBaseController {
 
         return ResultForFront.succ("");
     }
-    //2.合入PR
-    //3.关闭PR
-    //4.查看PR详情
-    //5.评审评论PR
+    //2.查看排序效果
+    //3.重新训练排序模型
+    //4.设置排序定时任务
 
 }
