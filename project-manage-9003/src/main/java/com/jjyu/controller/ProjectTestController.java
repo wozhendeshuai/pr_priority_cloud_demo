1fdd09ea27f6683d47a5f4f1b8d2a6fd0f93c94c
可及 <3183346767@qq.com>
2022-02-05 01:01:25 +0800

    测试接口

--- project-manage-9003/src/main/java/com/jjyu/controller/ProjectTestController.java
+++ project-manage-9003/src/main/java/com/jjyu/controller/ProjectTestController.java
@@ -1,6 +1,10 @@
 package com.jjyu.controller;
 
 
+import com.jjyu.service.*;
+import com.jjyu.utils.ResultForFront;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,8 +17,10 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
+
+@Api(value = "project/test", tags = {"项目测试接口"})
 @RestController
-@RequestMapping("project")
+@RequestMapping("project/test")
 @Slf4j
 public class ProjectTestController {
     @Value("${server.port}")
@@ -25,6 +31,7 @@ public class ProjectTestController {
     private RestTemplate restTemplate;
 
     //@RequestParam("prId") String prId,@RequestParam("fileId") String fileId
+    @ApiOperation(value = "测试port", notes = "testport")
     @GetMapping("/testport")
     public Map<String, Object> findPRselfAndFile() {
         log.info("=============ProjectTestController调用到啦");
@@ -39,4 +46,50 @@ public class ProjectTestController {
 
         return map;
     }
+
+    @Autowired
+    private RepoBaseService repoBaseService;
+
+    @ApiOperation(value = "listRepo", notes = "listRepo")
+    @GetMapping("/listRepo")
+    public ResultForFront listRepo() {
+        return ResultForFront.succ(repoBaseService.list());
+    }
+
+    @Autowired
+    private RepoDayService repoDayService;
+
+    @ApiOperation(value = "listRepoDay", notes = "listRepoDay")
+    @GetMapping("/listRepoDay")
+    public ResultForFront listRepoDay() {
+        return ResultForFront.succ(repoDayService.list());
+    }
+
+    @Autowired
+    private TeamService teamService;
+
+    @ApiOperation(value = "listTeam", notes = "listTeam")
+    @GetMapping("/listTeam")
+    public ResultForFront listTeam() {
+        return ResultForFront.succ(teamService.list());
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation(value = "listUser", notes = "listUser")
+    @GetMapping("/listUser")
+    public ResultForFront listUser() {
+        return ResultForFront.succ(userService.list());
+    }
+
+    @Autowired
+    private UserTeamService userTeamService;
+
+    @ApiOperation(value = "listUserTeam", notes = "listUserTeam")
+    @GetMapping("/listUserTeam")
+    public ResultForFront listUserTeam() {
+        return ResultForFront.succ(userTeamService.list());
+    }
+
 }
