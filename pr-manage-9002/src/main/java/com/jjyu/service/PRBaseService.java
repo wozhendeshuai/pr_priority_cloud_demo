b5f52409a274e86401f802fd3df9a1710d8deae3
可及 <3183346767@qq.com>
2022-02-05 20:01:04 +0800

    测试接口

--- pr-manage-9002/src/main/java/com/jjyu/service/PRBaseService.java
+++ pr-manage-9002/src/main/java/com/jjyu/service/PRBaseService.java
@@ -5,25 +5,20 @@ import com.jjyu.entity.PRDetail;
 
 public interface PRBaseService {
     //1.新建PR
-    public boolean newPR(String userName, String repoName);
+    boolean newPR(String userName, String repoName);
 
     //2.合入PR
-
-    public boolean mergePR(String userName, String prNumber, String repoName);
+    boolean mergePR(String userName, String prNumber, String repoName);
 
     //3.关闭PR
-
-    public boolean closePR(String userName, String prNumber, String repoName);
+    boolean closePR(String userName, String prNumber, String repoName);
 
     //4.查看PR详情
-
-    public PRDetail prDetail(String prNumber, String repoName);
+    PRDetail prDetail(String prNumber, String repoName);
 
     //5.评论PR
-
-    public boolean commentPR(String userName, String prNumber, String repoName, String commentContent);
+    boolean commentPR(String userName, String prNumber, String repoName, String commentContent);
 
     //5.评审PR
-
-    public boolean reviewPR(String userName, String prNumber, String repoName, String reviewContent);
+    boolean reviewPR(String userName, String prNumber, String repoName, String reviewContent);
 }
