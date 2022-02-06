a60e656677e87517faced09d803ab1b5747b8c78
可及 <3183346767@qq.com>
2022-02-06 11:30:06 +0800

    测试接口

---
+++ pr-manage-9002/src/main/java/com/jjyu/mapper/UserOperationMapper.java
@@ -0,0 +1,8 @@
+package com.jjyu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jjyu.entity.PRTask;
+import com.jjyu.entity.UserOperationEntity;
+
+public interface UserOperationMapper extends BaseMapper<UserOperationEntity> {
+}
---
+++ pr-manage-9002/src/main/java/com/jjyu/service/impl/UserOperationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.jjyu.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jjyu.entity.PRTask;
+
+import com.jjyu.entity.UserOperationEntity;
+import com.jjyu.mapper.UserOperationMapper;
+import com.jjyu.service.UserOperationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserOperationServiceImpl extends ServiceImpl<UserOperationMapper, UserOperationEntity> implements UserOperationService {
+
+}
